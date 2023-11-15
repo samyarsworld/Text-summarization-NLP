82ac79cdff390ea1d17e59f6788ac2c8b43c15ff
Samyar Farjam <samyarfarjam@outlook.com>
2023-11-15 13:50:15 -0500

    Add build_template to iniitialize project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Text-summarization-NLP
+
+### Project plan
+
+1 - Build project initialization setup using setuptools, os, and pathlib.
+
+1.1 - Include logging feature.
+
+2 - Build configuration class tools for different stages deep learning design to have one source of truth.
+
+3 - Build model by importing data, building dataset, network, training, evaluation, and prediction pipelines.
+
+4 - Keep track of the project on github and use github action for CD/CI and deployment.
+
+5 - Containerize project using Docker.
+
+6 - Deploy the model on AWS using S3, EC2, and possibly EKS service.
---
+++ build_template.py
@@ -0,0 +1,45 @@
+import os
+import logging
+
+# Setup information level (e.g., WARNING, ERROR, CRITICAL) logging
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_title = "text-summarization-NLP"
+
+# Project structure initialization
+files_and_dirs = [
+    "setup.py",
+    "requirements.txt",
+    "app.py",
+    "main.py",
+    f"src/{project_title}/__init__.py",
+    f"src/{project_title}/components/__init__.py",
+    f"src/{project_title}/utils/__init__.py",
+    f"src/{project_title}/utils/common.py",
+    f"src/{project_title}/logging/__init__.py",
+    f"src/{project_title}/config/__init__.py",
+    f"src/{project_title}/config/config.py",
+    f"src/{project_title}/pipeline/__init__.py",
+    f"src/{project_title}/entity/__init__.py",
+    f"src/{project_title}/constants/__init__.py",
+    "config/config.yaml",
+    "parameters.yaml",
+    "testing/test.ipynb",
+    "Dockerfile",
+    ".github/workflows/.gitkeep",
+]
+
+logging.info(f"Initializing project template started:")
+for path in files_and_dirs:
+    dir, file = os.path.split(path)
+
+    if dir and not os.path.exists(dir):
+        os.makedirs(dir, exist_ok=True)
+        logging.info(f"Creating directory:{dir}")
+
+    if path and not os.path.exists(path):
+        with open(path, "w") as f:
+            logging.info(f"Creating the file {file}")
+
+logging.info(f"Initializing project template finished:")
+
