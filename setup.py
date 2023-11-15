32d2ed123cdbe27fb3c5bbd3993a9c56b5040ad1
Samyar Farjam <samyarfarjam@outlook.com>
2023-11-15 15:38:49 -0500

    Add common utils

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
 
 __version__ = "0.0.0"
 
